ba884ae202d2ef02ae00e49d69d359c3264ba0a2
Greg Kemnitz <gjkemnitz@gmail.com>
2022-08-12 14:31:33 -0700

    get it in

---
+++ etl_wiz/include/etl_wiz.h
@@ -0,0 +1,8 @@
+#ifndef ETL_WIZ_H_INCL
+#define ETL_WIZ_H_INCL
+
+/* This is the main include file for etl_wiz */
+
+int foo = 0;
+
+#endif /* ETL_WIZ_H_INCL */
---
+++ etl_wiz/src/entry_point.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "etl_wiz.h"
+
+int
+main(int argc, char **argv)
+
+{
+  printf("hello world\n");
+  
+  return 0;
+}
